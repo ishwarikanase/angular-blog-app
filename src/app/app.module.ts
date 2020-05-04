3e59c4d825bc9c6b092afd612346ee2e0f5d9435
Ishwari Kanase <ishwari.kanase@gmail.com>
2020-05-04 16:05:23 +0530

    header homePage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HeaderComponent,
+    HomePageComponent,
+    
   ],
   imports: [
     BrowserModule,
